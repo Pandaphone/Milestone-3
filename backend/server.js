4e80fc49cc4f1da35ba42159e70ce1fa89b559f7
Pandaphone <shawngreene075@gmail.com>
2022-08-19 14:53:27 -0400

    More backend work

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 4000
+
+const app = express()
+
+app.get('/api/books', (req, res) => {
+    res.status(200).json({message: 'Hey!'})
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
