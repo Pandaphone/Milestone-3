1486a9a4551cb578efd34cc1d480976c63b3c2a7
Pandaphone <shawngreene075@gmail.com>
2022-08-19 22:43:58 -0400

    Changes

--- backend/Controllers/pController.js
+++ backend/Controllers/pController.js
@@ -1,14 +1,16 @@
+const asyncHandler = require('express-async-handler')
+
 //Gets a list of user's Personas via a GET Request. 
 //Route: /api/persona/
 //Private.
-const getPersonas = (req, res) => {
+const getPersonas = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Here are your personas!'})
-}
+})
 
 //Creates a new user Persona via a POST Request. 
 //Route: /api/persona/
 //Private.
-const createPersona = (req, res) => {
+const createPersona = asyncHandler(async (req, res) => {
     if(!req.body.text){
        res.status(400)
        throw new Error('Submit a proper persona!')
@@ -16,21 +18,21 @@ const createPersona = (req, res) => {
     else{
         res.status(200).json({ message: 'Create a persona!'})
     }
-}
+})
 
 //Updates the information of a user's Persona via a PUT Request. 
 // Route: /api/persona/:id
 //Private.
-const updatePersona = (req, res) => {
+const updatePersona = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update! ${req.params.id}`})
-}
+})
 
 //Deletes a user's Persona via a DELETE Request. 
 //Route: /api/persona/:id
 //Private.
-const deletePersona = (req, res) => {
+const deletePersona = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete! ${req.params.id}`})
-}
+})
 
 
 module.exports = {
