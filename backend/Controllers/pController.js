187f7826e9ee4e98f413d6804c82db3244277e3d
Pandaphone <shawngreene075@gmail.com>
2022-08-19 20:52:54 -0400

    Finished Controller.

--- backend/Controllers/pController.js
+++ backend/Controllers/pController.js
@@ -1,7 +1,35 @@
+//Gets a list of user's Personas via a GET Request. 
+//Route: /api/persona/
+//Private.
 const getPersonas = (req, res) => {
     res.status(200).json({ message: 'Here are your personas!'})
 }
 
+//Creates a new user Persona via a POST Request. 
+//Route: /api/persona/
+//Private.
+const createPersona = (req, res) => {
+    res.status(200).json({ message: 'Create a persona!'})
+}
+
+//Updates the information of a user's Persona via a PUT Request. 
+// Route: /api/persona/:id
+//Private.
+const updatePersona = (req, res) => {
+    res.status(200).json({ message: `Update! ${req.params.id}`})
+}
+
+//Deletes a user's Persona via a DELETE Request. 
+//Route: /api/persona/:id
+//Private.
+const deletePersona = (req, res) => {
+    res.status(200).json({ message: `Delete! ${req.params.id}`})
+}
+
+
 module.exports = {
     getPersonas,
+    createPersona,
+    updatePersona,
+    deletePersona,
 }
