5c8553b6f35fd4325fe64896b0c1bc3815865fa7
Pandaphone <shawngreene075@gmail.com>
2022-08-26 14:59:05 -0400

    Login page work

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const { email, password } = formData
+
+    //Allows for typing within the input boxes
+    const onnChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+    //Ignores the default submit actions
+    const onnSubmit = (e) => {
+        e.preventDefault()
+    }
+
+
+  return <>
+    <section className="heading">
+        <h1>
+            Login!
+        </h1>
+        <p>Login to Persona!</p>
+    </section>
+
+    <section className="form">
+        <form onSubmit={onnSubmit}>
+            <div className="form-group">
+             <input 
+             type="email" 
+             className="form-control" 
+             id='email' 
+             name='email' value={email} 
+             placeholder='Enter an email!' 
+             onChange={onnChange} 
+            />
+            </div>
+
+            <div className="form-group">
+             <input 
+             type="password" 
+             className="form-control" 
+             id='password' 
+             name='password' value={password} 
+             placeholder='Enter a password!' 
+             onChange={onnChange} 
+            />
+            </div>
+
+            <div className="form-group">
+             <button type="submit" className='btn btn-block'>Submit!</button>
+            </div>
+        </form>
+    </section>
+  </>
+  
 }
 
 export default Login
