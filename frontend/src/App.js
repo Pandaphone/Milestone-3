0ca7ee4de2b5ac8c34a66a466b8baa7f96b2e5ce
Pandaphone <shawngreene075@gmail.com>
2022-08-26 13:20:23 -0400

    Testing Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div>
-      
+      <Button>This is a button!</Button>
     </div>
   );
 }
