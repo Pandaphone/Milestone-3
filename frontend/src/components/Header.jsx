ea48a0ed7543f91cd299d30296590013f3a7bf41
Pandaphone <shawngreene075@gmail.com>
2022-08-26 20:09:34 -0400

    Logout functionality working! 10+ mins on one missing work...ack

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {logout, reset} from '../features/auth/authSlice' 
 
 function Header() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
   return (
     <header className='header'>
         <div className="logo">
@@ -9,12 +21,28 @@ function Header() {
         </div>
 
         <ul>
-            <Link to='/login'>
-                Login
-            </Link>
-            <Link to='/register'>
-                Register
-            </Link>
+            {user ? (
+            <li>
+             <button className="btn" onClick={onLogout}>
+                 Logout
+                 </button>
+             </li>
+             ) : (
+             <>
+             <li>
+             <Link to='/login'>
+                 Login
+             </Link>
+             </li>
+ 
+             <li>
+             <Link to='/register'>
+                 Register
+             </Link>
+             </li>
+             </>
+             )}
+   
         </ul>
     </header>
   )
