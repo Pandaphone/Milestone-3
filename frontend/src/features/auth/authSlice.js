6b4705a88a823e1dd70912cab2ecc70df34d070f
Pandaphone <shawngreene075@gmail.com>
2022-08-26 20:25:37 -0400

    Started Login functionality

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -13,10 +13,25 @@ const initialState = {
 }
 
 //Register a user
-//Makes a request and sends an error message in response
+//Makes a request and sends an error message in response if the user data isn't there
 export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
     try {
         return await authService.register(user)
+    } catch (error) {
+        const message = 
+        (error.response && 
+            error.response.data && 
+            error.response.data.message) || 
+            error.message || 
+            error.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+//Logs a user in
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
+    try {
+        return await authService.login(user)
     } catch (error) {
         const message = 
         (error.response && 
@@ -61,10 +76,24 @@ export const authSlice = createSlice({
                 state.isError = true
                 state.message = action.payload
                 state.user = null
+            }).addCase(login.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+                state.user = null
             })
             .addCase(logout.fulfilled, (state) => {
                 state.user = null
             })
+            
     }
 })
 
